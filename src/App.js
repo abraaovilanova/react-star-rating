a89f73accdd6c45f35441755878045094fc7ac5a
Abraão Vila Nova <73487632+abraaovilanova@users.noreply.github.com>
2023-02-10 11:02:23 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import StarRating from "./componentes/StarRating";
+
+export default function App() {
+  return (
+    <div className="App">
+      <StarRating />
+    </div>
+  );
+}
